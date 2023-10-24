17e9a8e77e65508013776e172b708206440bdc3e
mabilisasandile <mabilisasandile@gmail.com>
2023-10-24 22:42:26 +0200

    Second commit

--- app.js
+++ app.js
@@ -17,3 +17,26 @@ admin.auth().createCustomToken(uid, additionalClaims)
 .catch((error) => {
     console.log('Error creating custom token:', error);
 })
+
+
+//JSON output for successfully adding new user
+
+// {
+//     "uid": "Z0XxFUZcJAO7ik8Vw6HR5lJFUeZ2",
+//     "email": "sandile@gmail.com",
+//     "emailVerified": false,
+//     "disabled": false,
+//     "metadata": {
+//         "lastSignInTime": null,
+//         "creationTime": "Tue, 24 Oct 2023 19:16:44 GMT",
+//         "lastRefreshTime": null
+//     },
+//     "tokensValidAfterTime": "Tue, 24 Oct 2023 19:16:44 GMT",
+//     "providerData": [
+//         {
+//             "uid": "sandile@gmail.com",
+//             "email": "sandile@gmail.com",
+//             "providerId": "password"
+//         }
+//     ]
+// }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+
 const port = process.env.PORT || 4000;
 
 const serviceAccount = require('./serviceAccountKey.json'); // Key downloaded from Firebase Console
@@ -17,21 +18,34 @@ admin.initializeApp({     // Initialize Firebase Admin SDK
   databaseURL: "https://edutech-app-eecfd-default-rtdb.firebaseio.com"
 });
 
-app.use(cookieParser());
+// app.use(cookieParser());
+
+// Add body parsing middleware
+app.use(express.json());
+//express - Creates an Express application. The express() function is a top-level function exported by the express module.
+//json - Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to the admin dashboard!');
 });
 
+
+
+
+
 // Add necessary middleware
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
+
 
 
 
 //Create new user
 app.post('/', async (req, res) => {
-  console.log(req.body);
-  return 
+  
   const email = req.body.email;
   const password = req.body.password;
 
@@ -50,9 +64,16 @@ app.post('/', async (req, res) => {
 
 
 
+
+app.get('/add-admin', (req, res) => {
+  res.send('Admin settings dashboard!');
+});
+
 // Add a new admin endpoint
-app.post('/add-user', (req, res) => {
+// adding admin privileges to a user by setting custom claims using the Firebase Authentication SDK
+app.post('/add-admin', (req, res) => {
   const email = req.body.email; // Email of the new admin
+
   // Add custom admin claims to the user
   admin
     .auth()
